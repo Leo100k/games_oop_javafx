e6fd19a55ac8478f20a562e7c96ecd471d06044f
Leo100k <lemail2004@mail.ru>
2023-11-03 14:37:33 +0300

    3. Каркас шахматной доски [#792]

---
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.chess;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogicTest {
+
+    @Test
+    public void whenMoveThenFigureNotFoundException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenOccupiedCellException()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        new BishopBlack(Cell.C1);
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new PawnBlack(Cell.D2));
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Ход невозможен, слон вам не конь");
+    }
+}
